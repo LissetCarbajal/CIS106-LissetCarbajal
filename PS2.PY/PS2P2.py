414ff46d949ddcfcaf72844262064dc465f901a0
LissetCarbajal <l_carbajal1@mail.harpercollege.edu>
2024-10-04 10:57:21 -0500

    Add files via upload

---
+++ PS2.PY/PS2P2.py
@@ -0,0 +1,8 @@
+print ( "Enter Your Name")
+name= input()
+print ( "Enter your midterm grade")
+midterm = float(input())
+print ( "Enter your final grade")
+final = float(input())
+grade = midterm + final
+print ( name + "Your final course grade is "+ str(grade))
