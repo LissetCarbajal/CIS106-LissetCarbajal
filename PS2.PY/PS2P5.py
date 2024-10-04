8c5fc97e1df7b28eeb95a6c5d23910597786a0f2
LissetCarbajal <l_carbajal1@mail.harpercollege.edu>
2024-10-04 10:58:33 -0500

    Add files via upload

---
+++ PS2.PY/PS2P5.py
@@ -0,0 +1,6 @@
+print ( "Enter a radius of a circle" )
+radius = float(input())  
+area = 3.174 * radius * radius
+print ( "The area of the circle is " + str(area))
+perimeter = 2 * 3.174 * radius
+print ( "The perimeter of the circle is " + str(per))
