7bcd9b2f735574d4e519ad5452f5e0c9c3ad470a
LissetCarbajal <l_carbajal1@mail.harpercollege.edu>
2024-10-04 10:58:06 -0500

    Add files via upload

---
+++ PS2.PY/PS2P4.py
@@ -0,0 +1,8 @@
+print ( " Enter make, model, msrp amount and discount percent")
+make = input()
+model = input() 
+msrp = float(input())
+discount = float(input())
+amount off = msrp * discount 
+print ( " The amount off is " + str(amount off))
+print ( " The final price is " + str(msrp - amount off) + make + model + msrp + discount percent + amount off + discounted price )
