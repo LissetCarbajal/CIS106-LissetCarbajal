d888ff0e8ae0afd085ef2ff2642f9da3929c86d0
LissetCarbajal <l_carbajal1@mail.harpercollege.edu>
2024-10-11 15:48:59 -0500

    Add files via upload

---
+++ PS3.PY/PS3P1.py
@@ -0,0 +1,8 @@
+#This program will compute total points
+print ( "Enter last name " )
+last_name = str(input())
+Exam1 = float(input( "First exam score: "))
+Exam2 = float(input( "Second exam score: "))
+totalpoints = Exam1*.6+Exam2*.4
+print( "last_name:" , last_name)
+print( "totalpoints:", totalpoints)
