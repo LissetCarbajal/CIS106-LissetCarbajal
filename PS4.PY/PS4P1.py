06401c9b482e30fc0fbe785121c6d4da808410f6
LissetCarbajal <l_carbajal1@mail.harpercollege.edu>
2024-10-18 15:18:41 -0500

    Add files via upload

---
+++ PS4.PY/PS4P1.py
@@ -0,0 +1,12 @@
+print("Enter a quantity of an item")
+quan=int(input())
+if item quantity == ">=1000":
+  print("unit price is $3.00"))
+else:
+  print("unit price is $5.00")
+extended price = quan*unitprice
+print("extended price is $",extended price)
+tax = extended price * 0.07
+print("tax is $",tax)
+total = extended price + tax
+print("total is $",total)
