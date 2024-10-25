a9c533e5b1c7321faaf38df01c42462f235cf501
LissetCarbajal <l_carbajal1@mail.harpercollege.edu>
2024-10-25 15:20:03 -0500

    Add files via upload

---
+++ PS5.PY/PS5P3.py
@@ -0,0 +1,16 @@
+print ("Enter the principle amount of a CD")
+principle = float(input())
+print ("Enter the number of years to maturity")
+years = int(input())
+if principle > $100,000 and years == 5: interest rate == 0.06
+else
+if principle $50,000 to $100,000 and years == 10: interest rate == 0.05
+else
+if principle $50,000 to $100,000 and years == 5: interest rate == 0.04
+else
+if any other principle and years == 5: interest rate == 0.02
+#compute interest
+interest amount for first year = principle * interest rate
+interest rate = interest amount for first year * 0.5
+principle = principle + interest amount for first year
+print (interest amount for first year, interest amount for first year, principle))
