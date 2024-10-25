e5ef68f7c884b0dc05506f52a3807b183722af1b
LissetCarbajal <l_carbajal1@mail.harpercollege.edu>
2024-10-25 15:18:58 -0500

    Add files via upload

---
+++ PS5.PY/PS5P1.py
@@ -0,0 +1,15 @@
+print(quantity of widgets)
+quantity = int(input())
+print(price of widgets)
+print(extended price of widgets)
+extended price of widgets = quantity of widgets * price of widgets
+quantity of widgets>10000 price of widgets = $10.00
+quantity of widgets>5000 to 10000 price of widgets = $20.00
+quantity of widgets below 5000 = $30.00
+print(quantity of widgets * price of widgets)
+tax=extended price of widgets * 0.07
+print(tax)
+total=extended price of widgets + tax
+print(total,)
+print(extended price of widgets, extended price of widgets)
+print(tax amount, tax amount)
