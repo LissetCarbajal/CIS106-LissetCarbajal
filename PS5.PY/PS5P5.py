2b4ebb71ac1a014e9a1adfd23d2cd659c3b57d64
LissetCarbajal <l_carbajal1@mail.harpercollege.edu>
2024-10-25 15:20:39 -0500

    Add files via upload

---
+++ PS5.PY/PS5P5.py
@@ -0,0 +1,16 @@
+print ("Enter employee last name")
+lastname = input()
+print ("salary")
+salary = float(input())
+print ("job level")
+joblevel = int(input())
+if job level == 10 and above:
+  bonusrate = 0.25
+if job level == 5 to 9:
+  bonusrate = 0.20
+if all others:
+  bonusrate = 0.10
+#compute bonus
+bonus = salary * bonusrate
+#print results
+print (lastname,bonus)
