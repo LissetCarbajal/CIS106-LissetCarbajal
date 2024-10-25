701dbb219558c6c00356d8fb270aff9a37233d8a
LissetCarbajal <l_carbajal1@mail.harpercollege.edu>
2024-10-25 15:20:23 -0500

    Add files via upload

---
+++ PS5.PY/PS5P4.py
@@ -0,0 +1,13 @@
+print ("Enter number of concert tickets")
+tickets = int(input())
+if quantity >= 25:
+  price = 50
+if quantity = 10 to 24:
+  price = 60
+if quantity = 5 to 9:
+  price = 70
+if quantity < 5:
+  price = 75
+#compute price
+total cost = quantity * price
+print (number of tickets, price per ticket, total cost)
