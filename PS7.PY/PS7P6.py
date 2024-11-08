67c8f02249f392a86506ccbed0589e88c2354cbc
LissetCarbajal <l_carbajal1@mail.harpercollege.edu>
2024-11-08 15:40:16 -0600

    Add files via upload

---
+++ PS7.PY/PS7P6.py
@@ -0,0 +1,10 @@
+# Initialize the first two Fibonacci numbers
+# a, b = 1, 1
+print (a)
+print (b)
+# Loop to generate the next Fibonacci numbers
+for i in range (1, 20):
+# Calculate the next Fibonacci number
+a, b = b, a + b
+# Print the next Fibonacci number
+print (b))
